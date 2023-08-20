34857d580c1ab684b4f41070150e6f67624763ef
8030046 <8030046@qq.com>
2023-08-20 15:22:34 +0800

    123

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>你好，世界</div>
+  </React.StrictMode>
+);
+
+
